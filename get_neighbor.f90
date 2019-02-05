7a7a27107a4df18dbc07826bc0c790d368b0ff78
TakayukiKanki <tkanki@aori.u-tokyo.ac.jp>
2019-02-06 01:02:24 +0900

    0206

--- get_neighbor.f90
+++ get_neighbor.f90
@@ -42,9 +42,10 @@ contains
 
     !ループ1のface_indexを取得する
     !iは深いほうが速いだろう．検討
-    do i=1, faces_n
-        do k=1, size(vertice_index_0)
-          do j=1, 3
+
+    do k=1, size(vertice_index_0)
+      do i=1, faces_n
+        do j=1, 3
           !面iを構成する座標にvertice_index_0と同じ座標が含まれているならば，
           if(faces_matrix(i,j)==vertice_index_0(k)) then
            !append的にインデックスを追加したい
@@ -94,9 +95,9 @@ contains
 
     !ループ1のface_indexを取得する
     !iは深いほうが速いだろう．検討
-    do i=1, faces_n
-        do k=1, size(vertice_index_0)
-          do j=1, 3
+    do k=1, size(vertice_index_0)
+      do i=1, faces_n
+        do j=1, 3
           !面iを構成する座標にvertice_index_0と同じ座標が含まれているならば，
           if(faces_matrix(i,j)==vertice_index_0(k)) then
            !append的にインデックスを追加したい
@@ -131,9 +132,9 @@ contains
 
     !ループ2のface_indexを取得する
     !iは深いほうが速いだろう．検討
-    do i=1, faces_n
+    do j=1, 3
+      do i=1, faces_n
         do k=1, vertice_n
-          do j=1, 3
           !vertice_index_1でチェックされた点kに対して，
           !面iを構成する座標のどれかに点kと同じ座標が含まれているならば，その面を追加する
           if(vertice_index_1(k)==1 .and. faces_matrix(i,j)==k) then
